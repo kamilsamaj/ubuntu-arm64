809c827113b7a849c57cf9f772dbb1312dee0584
kamilsamaj <kamilsamaj@gmail.com>
2020-11-21 10:37:30 +0100

    add cloudinit

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 # Install prerequisites
 ```
+sudo apt install -y qemu-system-arm qemu-efi-aarch64 qemu-utils cloud-image-utils
+```
 
 # Create UEFI volumes
 ```
@@ -10,13 +12,39 @@ dd if=/usr/share/qemu-efi/QEMU_EFI.fd of=flash0.img conv=notrunc
 dd if=/dev/zero of=flash1.img bs=1M count=64
 ```
 
+# Create CloudInit volume
+```
+cat > user-data <<EOF
+#cloud-config
+password: your_password
+chpasswd: { expire: False }
+ssh_pwauth: True
+EOF
+
+cloud-localds user-data.img user-data
+
+# user-data.img MUST come after the rootfs.
+qemu-system-x86_64 \
+-drive file=ubuntu-18.04-server-cloudimg-amd64.img,format=qcow2 \
+-drive file=user-data.img,format=raw \
+-m 1G
+...
+```
+
 # Download server image
 http://cloud-images.ubuntu.com/focal/current/
 
-# Start the intance
+# Start the instance
 ```
 ./start.sh
 ```
 
+# Reset password after first login
+```
+passwd
+```
+
 # Reference
 https://wiki.ubuntu.com/ARM64/QEMU
+https://futurewei-cloud.github.io/ARM-Datacenter/qemu/how-to-launch-aarch64-vm/
+https://stackoverflow.com/questions/29137679/login-credentials-of-ubuntu-cloud-server-image
--- start.sh
+++ start.sh
@@ -8,14 +8,28 @@ if ! ip a show tap0 &>/dev/null; then
 fi
 
     #-netdev type=tap,id=net0 \
-sudo qemu-system-aarch64 \
-    -m 1024 \
-    -cpu cortex-a57 \
-    -M virt \
+#sudo qemu-system-aarch64 \
+#    -M virt \
+#    -cpu cortex-a57 \
+#    -m 2048 \
+#    -pflash flash0.img \
+#    -pflash flash1.img \
+#    -drive if=none,file=focal-server-cloudimg-arm64.img,id=hd0 \
+#    -device virtio-blk-device,drive=hd0 \
+#    -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
+#    -device vmxnet3,netdev=net0,id=net0,mac=01:02:03:04:05:05 \
+#    -nographic
+
+qemu-system-aarch64 \
     -nographic \
-    -pflash flash0.img \
-    -pflash flash1.img \
-    -drive if=none,file=focal-server-cloudimg-arm64.img,id=hd0 \
-    -device virtio-blk-device,drive=hd0 \
-    -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
-    -device vmxnet3,netdev=net0,id=net0,mac=01:02:03:04:05:05
+    -machine virt,gic-version=max \
+    -m 2048M \
+    -cpu max \
+    -smp 4 \
+    -netdev user,id=vnet,hostfwd=:127.0.0.1:5022-:22 \
+    -device virtio-net-pci,netdev=vnet \
+    -drive file=focal-server-cloudimg-arm64.img,if=none,id=drive0,cache=writeback \
+    -drive file=user-data.img,format=raw \
+    -device virtio-blk,drive=drive0,bootindex=0 \
+    -drive file=flash0.img,format=raw,if=pflash \
+    -drive file=flash1.img,format=raw,if=pflash
